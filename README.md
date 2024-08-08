a0987fb81cd0a33cca1af016ddc87faa4605037c
caires <caires.santos@codegaia.io>
2024-08-08 16:37:31 +0200

    Initial homebrew configuration
    Not super clean/isolated, but I can try to move the configuration.nix to
    darwin later on

--- README.md
+++ README.md
@@ -18,6 +18,17 @@ nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 
 4. Add the following to configuration.nix to enable nix-command and flakes features:
 
+Add devenv to system packages:
+
+```console
+environment.systemPackages =
+[
+    pkgs.devenv
+    pkgs.vim
+];
+```
+
+Enable flakes:
 ```console
 nix.settings.experimental-features = [ "nix-command" "flakes" ];
 ```
@@ -28,3 +39,20 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
 Would you like to manage <darwin> with nix-channel? [y/n]
 ```
 
+6. Execute and install all the packages (this might take a while)
+
+```console
+just darwin-switch
+```
+
+## Todo
+
+Move the Mac Applications to nix:
+
+1password
+cleanshot
+font-jetbrains-mono-nerd-font
+hammerspoon
+hiddenbar
+stats
+yubico-yubikey-manager
--- module/configuration.nix
+++ module/configuration.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   # add more system settings here
   nix = {
@@ -13,5 +15,45 @@
       warn-dirty = false;
     };
   };
+
   programs.zsh.enable = true;
+
+  fonts.packages = [
+    pkgs.atkinson-hyperlegible
+    pkgs.jetbrains-mono
+  ];
+
+  homebrew = {
+      enable = true;
+
+      casks = [
+          #"1password"
+          "firefox"
+          #"hammerspoon"
+          "obsidian"
+      ];
+  };
+
+  system = {
+      defaults = {
+          dock = {
+              autohide = true;
+              orientation = "left";
+              show-process-indicators = false;
+              show-recents = false;
+              static-only = true;
+          };
+          finder = {
+              AppleShowAllExtensions = true;
+              FXDefaultSearchScope = "SCcf";
+              FXEnableExtensionChangeWarning = false;
+              ShowPathbar = true;
+          };
+          NSGlobalDomain = {
+              AppleKeyboardUIMode = 3;
+              "com.apple.keyboard.fnState" = true;
+              NSAutomaticWindowAnimationsEnabled = false;
+          };
+      };
+  };
 }
