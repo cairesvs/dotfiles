adc2449c9fbc387669e10a0b240af4a24abe8564
caires <cairesvs@gmail.com>
2025-08-30 20:07:54 +0200

    add lazyvim stuff

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  "sonph/onehalf",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Add the vim subdirectory to runtimepath
-    vim.opt.rtp:append(vim.fn.stdpath("data") .. "/lazy/onehalf/vim")
-    vim.cmd("colorscheme onehalfdark")
-  end,
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
